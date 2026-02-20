9c52b0d52a1c7c9a081cd22763ff20665a167c97
liverforpresent <dailynsteady@gmail.com>
2026-02-21 01:12:20 +0900

    feat: 회원탈퇴(소셜 로그인 연동 해제) 로직 구현

---
+++ src/main/kotlin/com/univent/api/iam/auth/user/application/service/UnlinkOAuthService.kt
@@ -0,0 +1,31 @@
+package com.univent.api.iam.auth.user.application.service
+
+import com.univent.api.common.exception.CustomException
+import com.univent.api.iam.auth.core.domain.AuthErrorCode
+import com.univent.api.iam.auth.core.infrastructure.oauth.OAuthProviderFactory
+import com.univent.api.iam.auth.user.application.UnlinkOAuthUseCase
+import com.univent.api.iam.auth.user.application.command.UnlinkOAuthCommand
+import com.univent.api.iam.auth.user.domain.AuthUserStore
+import com.univent.api.iam.user.UserApi
+import com.univent.api.iam.user.UserDto
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class UnlinkOAuthService(
+    private val authUserStore: AuthUserStore,
+    private val userApi: UserApi,
+    private val oAuthProviderFactory: OAuthProviderFactory
+): UnlinkOAuthUseCase {
+    @Transactional
+    override fun execute(command: UnlinkOAuthCommand) {
+        val authUser = authUserStore.loadByUserId(command.userId)
+            ?: throw CustomException(AuthErrorCode.AUTH_USER_NOT_FOUND)
+
+        userApi.deleteUser(UserDto.DeleteUserRequest(authUser.userId.value))
+
+        oAuthProviderFactory.getProvider(command.provider)
+
+        authUser.delete()
+    }
+}
