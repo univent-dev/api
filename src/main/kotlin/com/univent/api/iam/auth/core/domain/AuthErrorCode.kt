5922d28b9d048d2dcdfe970d6a79a8068620388c
liverforpresent <dailynsteady@gmail.com>
2026-02-21 21:46:04 +0900

    feat: Auth 예외 코드 정리

--- src/main/kotlin/com/univent/api/iam/auth/core/domain/AuthErrorCode.kt
+++ src/main/kotlin/com/univent/api/iam/auth/core/domain/AuthErrorCode.kt
@@ -11,5 +11,10 @@ enum class AuthErrorCode(
     AUTH_USER_ID_EMPTY(ErrorStatus.BAD_REQUEST, "사용자 ID는 비어 있을 수 없습니다."),
     AUTH_USER_NOT_FOUND(ErrorStatus.NOT_FOUND, "인증된 사용자를 찾을 수 없습니다."),
     AUTH_USER_ALREADY_EXISTS(ErrorStatus.CONFLICT, "이미 인증된 사용자가 존재합니다."),
-    INVALID_OAUTH_ID(ErrorStatus.BAD_REQUEST, "유효하지 않은 OAuth ID입니다.")
+    INVALID_OAUTH_ID(ErrorStatus.BAD_REQUEST, "유효하지 않은 OAuth ID입니다."),
+    AUTH_INVALID_REFRESH_TOKEN(ErrorStatus.BAD_REQUEST, "유효하지 않은 리프레시 토큰입니다."),
+    AUTH_INVALID_OAUTH_PROVIDER(ErrorStatus.BAD_REQUEST, "유효하지 않은 OAuth 제공자입니다."),
+    AUTH_INVALID_OAUTH_TOKEN(ErrorStatus.BAD_REQUEST, "유효하지 않은 OAuth 토큰입니다."),
+    AUTH_OAUTH_USER_NOT_FOUND(ErrorStatus.NOT_FOUND, "OAuth 사용자 정보를 찾을 수 없습니다."),
+    AUTH_OAUTH_USER_EMAIL_NOT_FOUND(ErrorStatus.NOT_FOUND, "OAuth 사용자 이메일 정보를 찾을 수 없습니다."),
 }
