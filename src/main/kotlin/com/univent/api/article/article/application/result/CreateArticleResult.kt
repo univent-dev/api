d0b4c506c24ae9e6058687793d3576369536f9ce
liverforpresent <dailynsteady@gmail.com>
2026-03-02 14:53:46 +0900

    feat: Article 생성 로직 구현

---
+++ src/main/kotlin/com/univent/api/article/article/application/result/CreateArticleResult.kt
@@ -0,0 +1,7 @@
+package com.univent.api.article.article.application.result
+
+import com.univent.api.common.core.domain.vo.identifier.ArticleId
+
+data class CreateArticleResult(
+    val articleId: ArticleId
+)
