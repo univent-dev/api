c7aec8b799a320e0243ad756cfd389224f7fc9b4
liverforpresent <dailynsteady@gmail.com>
2026-03-05 13:09:28 +0900

    feat: Scrap 추가 로직 구현

---
+++ src/main/kotlin/com/univent/api/article/scrap/application/service/AddScrapService.kt
@@ -0,0 +1,50 @@
+package com.univent.api.article.scrap.application.service
+
+import com.univent.api.article.article.ArticleApi
+import com.univent.api.article.article.ArticleDto
+import com.univent.api.article.scrap.application.AddScrapUseCase
+import com.univent.api.article.scrap.application.command.AddScrapCommand
+import com.univent.api.article.scrap.domain.Scrap
+import com.univent.api.article.scrap.domain.ScrapErrorCode
+import com.univent.api.article.scrap.domain.ScrapStore
+import com.univent.api.article.scrap.domain.event.ScrapAddedEvent
+import com.univent.api.common.core.domain.IdGenerator
+import com.univent.api.common.core.domain.vo.identifier.ScrapId
+import com.univent.api.common.exception.CustomException
+import org.springframework.context.ApplicationEventPublisher
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class AddScrapService(
+    private val scrapStore: ScrapStore,
+    private val articleApi: ArticleApi,
+    private val idGenerator: IdGenerator,
+    private val applicationEventPublisher: ApplicationEventPublisher
+) : AddScrapUseCase {
+    @Transactional
+    override fun execute(command: AddScrapCommand) {
+        val existingScrap = scrapStore.loadByArticleIdAndUserId(command.articleId, command.userId)
+        if (existingScrap != null) throw CustomException(ScrapErrorCode.SCRAP_ALREADY_EXISTS)
+
+        val article = articleApi.getArticleById(ArticleDto.GetArticleByIdRequest(command.articleId.value))
+
+        val scrap = Scrap.create(
+            id = ScrapId(idGenerator.generateId()),
+            articleId = command.articleId,
+            userId = command.userId
+        )
+
+        scrapStore.save(scrap)
+
+        articleApi.increaseScrapCount(ArticleDto.IncreaseScrapCountRequest(command.articleId.value))
+
+        applicationEventPublisher.publishEvent(
+            ScrapAddedEvent(
+                userId = command.userId.value,
+                articleId = command.articleId.value,
+                tags = article.tags
+            )
+        )
+    }
+}
