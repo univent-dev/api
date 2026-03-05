a833e98b255c9ff36c37bc9867a15a71dcd7d672
liverforpresent <dailynsteady@gmail.com>
2026-03-05 13:17:23 +0900

    feat: ArticleApi 구현

--- src/main/kotlin/com/univent/api/article/article/application/query/GetArticleByIdQuery.kt
+++ src/main/kotlin/com/univent/api/article/article/application/query/GetArticleByIdQuery.kt
@@ -1,5 +1,15 @@
 package com.univent.api.article.article.application.query
 
+import com.univent.api.article.article.ArticleDto
+
 data class GetArticleByIdQuery(
     val id: Long
-)
+) {
+    companion object {
+        fun fromDto(dto: ArticleDto.GetArticleByIdRequest): GetArticleByIdQuery {
+            return GetArticleByIdQuery(
+                id = dto.articleId
+            )
+        }
+    }
+}
