c131dc49eb8e4d54f911078315c435dc65fb3286
liverforpresent <dailynsteady@gmail.com>
2026-03-18 14:10:50 +0900

    feat: Scrap 앰플리튜드 이벤트 추적 로직 구현

---
+++ src/main/kotlin/com/univent/api/analytics/application/ScrapAnalyticsHandler.kt
@@ -0,0 +1,32 @@
+package com.univent.api.analytics.application
+
+import com.univent.api.common.core.domain.event.scrap.ScrapAddedEvent
+import com.univent.api.common.core.domain.event.scrap.ScrapDeletedEvent
+import mu.KotlinLogging
+import org.springframework.modulith.events.ApplicationModuleListener
+import org.springframework.stereotype.Component
+
+@Component
+class ScrapAnalyticsHandler(
+    private val eventTracker: EventTracker
+) {
+    private val logger = KotlinLogging.logger {}
+
+    @ApplicationModuleListener
+    fun onScrapAdded(event: ScrapAddedEvent) {
+        eventTracker.track(
+            userId = event.userId.toString(),
+            eventType = "Article Scrapped",
+            properties = mapOf("articleId" to event.articleId)
+        )
+    }
+
+    @ApplicationModuleListener
+    fun onScrapDeleted(event: ScrapDeletedEvent) {
+        eventTracker.track(
+            userId = event.userId.toString(),
+            eventType = "Article Unscrapped",
+            properties = mapOf("articleId" to event.articleId)
+        )
+    }
+}
